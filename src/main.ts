46f1bc024aef36f3b4c3bae3f0bdea8c2b353419
rorteza rostami <morteza9rostami@gmail.com>
2024-01-02 13:25:25 -0600

    first

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+//import './style.css'
+//import viteLogo from '/vite.svg'
+
+import module1 from "./modules/module1";
+
+function main() {
+  module1.main();
+}
+
+main();
---
+++ src/modules/module1.ts
@@ -0,0 +1,42 @@
+
+const module1 = {
+
+  lesson1() {
+    let result = null;
+    // bubble sort
+
+    // reverse array
+
+    // rotate array: left and right:
+    /* 
+    Array Rotation: Rotate the elements of an array left or right by a specified number of positions.
+
+
+    */
+
+    /* 
+    # duplicate removal
+
+    Duplicate Removal: Remove duplicates from an array while preserving the order of elements.
+
+
+    */
+
+    const duplicates = [1, 2, 4, 1, 2, 8, 8];
+
+    function removeDuplicate(arr: number[]) {
+
+      
+    }
+
+    result = removeDuplicate(duplicates);
+
+    console.log(result);
+  },
+
+  main() {
+    this.lesson1();
+  },
+}
+
+export default module1
