3c5cce7d33f4f838bcd80b9e9765a5e85f67e289
rorteza rostami <morteza9rostami@gmail.com>
2024-01-04 04:04:34 -0600

    master-coding-interview part-5

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@
 
 import module1 from "./modules/module1";
 import module2 from "./modules/module2";
+import module3 from "./modules/module3";
 
 function main() {
   //module1.main();
-  module2.main();
+  //module2.main();
+  module3.main();
+
 }
 
 main();
---
+++ src/modules/module3.ts
@@ -0,0 +1,17 @@
+
+const module3 = {
+
+  foo() {
+    let result = null;
+
+
+
+    console.log(result);
+  },
+
+  main() {
+    this.foo();
+  },
+}
+
+export default module3
